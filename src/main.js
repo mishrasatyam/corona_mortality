fe385c484a47c128b0c701259d91bd782479b0e0
Satyam Mishra <satyam.mishra975@gmail.com>
2020-08-05 23:58:02 +0530

    rounded off rate to 2 decimal places

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ async function get_data(){
 	let res = await fetch('https://disease.sh/v3/covid-19/countries')
 	let data = await res.json();
 	let total = data.length;
-	data.forEach(el => mortality_data.push({country:el.country,rate:(el.deaths/el.cases)*100}))
+	data.forEach(el => mortality_data.push({country:el.country,rate:((el.deaths/el.cases)*100).toFixed(2)}))
 	mortality_data.sort((a,b)=>{return b.rate-a.rate})
 	let i = total;
 	mortality_data.forEach(el => { el.rank=i;i-=1; })
