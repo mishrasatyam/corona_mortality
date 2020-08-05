78fb0b5d1b93da8470209a0af414e7672aa6ec1b
Satyam Mishra <satyam.mishra975@gmail.com>
2020-08-06 00:49:49 +0530

    added india tab

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,31 @@
 <script>
 	export let data_promise;
+	let tab="world"
 	let table_data = [];
 	let mortality_rate_data = [];
 	let search_text;
+	let search_state_text;
+	let state_table_data=[];
+	let india_mortality_rate_data = [];
 	async function get_data(){
-		mortality_rate_data = await data_promise;
+		let data = await data_promise;
+		mortality_rate_data = data.mortality_data;
+		india_mortality_rate_data = data.india_mortality_data;
 		table_data = mortality_rate_data
+		state_table_data = india_mortality_rate_data;
 	}
 	get_data()
 	function search(){
-		console.log(search_text)
 		table_data = mortality_rate_data.filter(el => el.country.toLowerCase().startsWith(search_text.toLowerCase()))
+	}
+	function search_state(){
+		state_table_data = india_mortality_rate_data.filter(el => el.state.toLowerCase().startsWith(search_state_text.toLowerCase()))
 	}
 </script>
 	<div class="container">
+	{#if tab=="world"}	
 	<h1 style="text-align:center">Mortality data</h1>
+	<h6 style="text-align:center" class="link" on:click={()=>tab='india'}>India</h6>
 	<div class="form-group">
 		<label>Search by country</label>
 		<input type="search" class="form-control" bind:value={search_text} on:keyup={search}>
@@ -31,12 +42,42 @@
 			{#each table_data as el}
 			<tr>
 				<td>{el.rank}</td>
+				{#if el.country=='India'}
+				<td class="link" on:click={()=>tab="india"}>{el.country}</td>
+				{:else}
 				<td>{el.country}</td>
+				{/if}
 				<td>{el.rate}</td>
 			</tr>
 			{/each}
 		</tbody>
 	</table>
+	{:else if tab=="india"}
+	<h1 style="text-align:center">Mortality data India</h1>
+	<h6 style="text-align:center" class="link" on:click={()=>tab='world'}>Go back</h6>
+	<div class="form-group">
+		<label>Search by state</label>
+		<input type="search" class="form-control" bind:value={search_state_text} on:keyup={search_state}>
+	</div>
+	<table class="table table-bordered">
+		<thead>
+		<tr>
+			<th>Rank</th>
+			<th>State/UT</th>
+			<th>Rate(%)</th>
+		</tr>
+	</thead>
+		<tbody>
+			{#each state_table_data as el}
+			<tr>
+				<td>{el.rank}</td>
+				<td>{el.state}</td>
+				<td>{el.rate}</td>
+			</tr>
+			{/each}
+		</tbody>
+	</table>
+	{/if}
 </div>
 	<style>
 
@@ -47,5 +88,9 @@
 		font-weight: 100;
 	}
 
+	.link{
+		color: blue;
+		cursor: pointer;
+	}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 import App from './App.svelte';
 import fetch from 'node-fetch';
+function percentage(a,b){
+	let val = (a/b)*100;
+	return (Number.isNaN(val)) ? 0.00 : val.toFixed(2);
+}
 async function get_data(){
 	let mortality_data = [];
 	let res = await fetch('https://disease.sh/v3/covid-19/countries')
 	let data = await res.json();
-	let total = data.length;
-	data.forEach(el => mortality_data.push({country:el.country,rate:((el.deaths/el.cases)*100).toFixed(2)}))
+	let i = data.length;
+	data.forEach(el => mortality_data.push({country:el.country,rate:percentage(el.deaths,el.cases)}))
 	mortality_data.sort((a,b)=>{return b.rate-a.rate})
-	let i = total;
 	mortality_data.forEach(el => { el.rank=i;i-=1; })
-	return mortality_data
+	let india_mortality_data = []
+	let res2 = await fetch('https://api.covid19india.org/data.json')
+	let data2 = (await res2.json()).statewise;
+	data2 = data2.filter(el => !(el.statecode=='UN' || el.statecode=='TT'))
+	let j = data2.length
+	
+	data2.forEach(el => india_mortality_data.push({state:el.state,rate:percentage(el.deaths,el.confirmed)}))
+	
+	india_mortality_data.sort((a,b) => {return b.rate-a.rate})
+	india_mortality_data.forEach(el =>{el.rank=j;j-=1})
+	console.log(india_mortality_data)
+	return {mortality_data,india_mortality_data}
 }
 const app = new App({
 	target: document.body,
