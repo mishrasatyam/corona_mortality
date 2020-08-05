6cdc59160c4ac68549cf89d2d6d19871b92471fc
Satyam Mishra <satyam.mishra975@gmail.com>
2020-08-05 23:55:25 +0530

    added bootstrap

--- src/App.svelte
+++ src/App.svelte
@@ -13,17 +13,20 @@
 		table_data = mortality_rate_data.filter(el => el.country.toLowerCase().startsWith(search_text.toLowerCase()))
 	}
 </script>
-
-<main>
-	<h1>Mortality data</h1>
-	<label>Search by country</label>
-	<input type="search" bind:value={search_text} on:keyup={search}>
-	<table>
+	<div class="container">
+	<h1 style="text-align:center">Mortality data</h1>
+	<div class="form-group">
+		<label>Search by country</label>
+		<input type="search" class="form-control" bind:value={search_text} on:keyup={search}>
+	</div>
+	<table class="table table-bordered">
+		<thead>
 		<tr>
 			<th>Rank</th>
 			<th>Country</th>
 			<th>Rate(%)</th>
 		</tr>
+	</thead>
 		<tbody>
 			{#each table_data as el}
 			<tr>
@@ -34,17 +37,8 @@
 			{/each}
 		</tbody>
 	</table>
-	
-
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+</div>
+	<style>
 
 	h1 {
 		color: #ff3e00;
@@ -53,36 +47,5 @@
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-	
-table {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
 
-table td, table th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-table tr:nth-child(even){background-color: #f2f2f2;}
-
-table tr:hover {background-color: #ddd;}
-
-table th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
-tr>th{
-	background-color: aliceblue;
-	color:black
-}
 </style>
