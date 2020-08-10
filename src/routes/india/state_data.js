ff443a6ba4bff5c9f894e10ac31f02457e4d80bf
Satyam Mishra <satyam.mishra975@gmail.com>
2020-08-11 00:41:39 +0530

    generate data if error or date is 6 hours late

--- src/routes/india/state_data.js
+++ src/routes/india/state_data.js
@@ -1,6 +1,22 @@
 import fs from "fs";
+import { diff_hours,update } from "../utils";
 
-export async function get(req,res){
-    const data = JSON.parse(await fs.promises.readFile('state_data.json'))
+export async function get(req,res){    
+    const now_date = new Date();
+    let data;
+    try{
+        let date = JSON.parse(await fs.promises.readFile('date.json'))
+        if(diff_hours(now_date,new Date(date.date))>6){
+            await update(now_date);
+        }
+    }catch(err){
+        await update(now_date);
+    }
+    try{
+        data = JSON.parse(await fs.promises.readFile('state_data.json'));
+    }catch(err){
+        await update(now_date);
+        data = JSON.parse(await fs.promises.readFile('state_data.json'));
+    }
     res.end(JSON.stringify(data))
 }
--- src/server.js
+++ src/server.js
@@ -2,90 +2,10 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-import fetch from 'node-fetch';
-import fs from 'fs'
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
-function percentage(a,b){
-	let val = (a/b)*100;
-	return (Number.isNaN(val)) ? 0.00 : val.toFixed(2);
-}
 
-function num_format(num){
-	return Number(num).toLocaleString('en-IN')
-}
-
-async function world_data(){
-	let mortality_data = [];
-	const url = 'https://disease.sh/v3/covid-19/countries';
-	const res = await fetch(url);
-	const data = await res.json();
-	let i = data.length;
-	data.forEach(el => mortality_data.push({country:el.country,death_rate:percentage(el.deaths,el.cases),death:num_format(el.deaths)
-		,active_rate:percentage(el.active,el.cases),active:num_format(el.active)
-		,recovered_rate:percentage(el.recovered,el.cases),recovered:num_format(el.recovered)
-	}))
-	mortality_data.sort((a,b)=>{return b.death_rate-a.death_rate})
-	mortality_data.forEach(el => { el.rank=i;i-=1; })
-	await fs.promises.writeFile('world_data.json',JSON.stringify({data:mortality_data,url},null,4))
-	// return {data:mortality_data,url}
-	
-}
-
-async function india_data(){
-	let mortality_data = [];
-	const url = 'https://api.covid19india.org/data.json';
-	const res = await fetch(url);
-	let data = (await res.json()).statewise;
-	data = data.filter(el => !(el.statecode=='UN' || el.statecode=='TT'))
-	data = data.filter(el => !(el.statecode=='UN' || el.statecode=='TT'))
-	let i = data.length
-	data.forEach(el => mortality_data.push({state:el.state,death_rate:percentage(el.deaths,el.confirmed),death:num_format(el.deaths)
-		,active_rate:percentage(el.active,el.confirmed),active:num_format(el.active)
-		,recovered_rate:percentage(el.recovered,el.confirmed),recovered:num_format(el.recovered)
-	}))
-	mortality_data.sort((a,b) => {return b.death_rate-a.death_rate})
-	mortality_data.forEach(el =>{el.rank=i;i-=1})
-	// return {data:mortality_data,url}
-	await fs.promises.writeFile('india_data.json',JSON.stringify({data:mortality_data,url},null,4))
-	
-}
-async function state_data(){
-	let final_state_data = {};
-	const url = 'https://api.covid19india.org/state_district_wise.json';
-	const res = await fetch(url);
-	const data = await res.json();
-	let state_list = Object.keys(data)
-	state_list.forEach(state =>{
-		if(state!='State Unassigned'){
-			let state_data = data[state];
-			let mortality_data = [];
-			const district_data = state_data.districtData;
-			Object.entries(district_data).forEach(el => {
-				let district = el[0];
-				let value = el[1];
-				if(district !='Unknown' && district !='Other State'){
-					mortality_data.push({district,death_rate:percentage(value.deceased,value.confirmed),death:num_format(value.deceased)
-					,active_rate:percentage(value.active,value.confirmed),active:num_format(value.active)
-					,recovered_rate:percentage(value.recovered,value.confirmed),recovered:num_format(value.recovered)
-					})
-				}
-			})
-			let i = mortality_data.length;
-			mortality_data.sort((a,b)=>{return b.death_rate-a.death_rate})
-			mortality_data.forEach(el => { el.rank=i;i-=1; })
-			final_state_data[state] = mortality_data
-		}	
-	})
-	await fs.promises.writeFile('state_data.json',JSON.stringify({data:final_state_data,url},null,4))
-	// return {data:state_data,url}	
-	
-}
 polka() // You can also use Express
-	.get('/update/',async (req,res)=>{
-		await Promise.all([world_data(),india_data(),state_data()])
-		res.end();
-	})
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
